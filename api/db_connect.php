a7b2664ca9bd039db80d53d8c4974b8e9610e6b5
George Nemukuyu <147068412+Cxtrazw@users.noreply.github.com>
2025-04-03 14:41:00 +0200

    Add api/db_connect.php 1743684057390

---
+++ api/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "acz_complaints";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
